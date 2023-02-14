8b598158cd108727ebf237915d834247411046b3
Julián <j.gakusei11@gmail.com>
2023-02-14 18:23:40 -0300

    TP Done

---
+++ SuperEmpresa/Persona.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperEmpresa
+{
+    /* 
+        Consigna:
+        ---------------------
+        Modelar la clase Persona con las siguientes condiciones:
+            - Atributos: nombre, e-mail, sueldo, sexo, peso y altura. 
+            * Pensar qué modificador de visibilidad es el más adecuado junto 
+            con el tipo de cada uno ya que no se debe acceder a sus atributos 
+            directamente.
+
+            - Implementar un constructor que defina todos los atributos por 
+            parámetro salvo el e-mail que se especifica luego.
+               
+            - Implementar los siguientes métodos:
+                + Todos los métodos Getters, No implementar Setters.
+                + CrearEmail():  Crea el e-mail de la persona a partir del 
+                nombre + @superempresa.com. Este método es privado y se llama 
+                por medio del constructor.
+    */
+
+    class Persona
+    {
+        private string _nombre;
+        private string _email;
+        private float _sueldo;
+        private string _sexo;
+        private float _peso;
+        private float _altura;
+
+        public Persona(string nombre, float sueldo, string sexo, float peso, float altura)
+        {
+            _nombre = nombre;
+            _sueldo = sueldo;
+            _sexo = sexo;
+            _peso = peso;
+            _altura = altura;
+
+            CrearEmail();
+        }
+
+        public string GetNombre() { return _nombre; }
+        public string GetEmail() { return _email; }
+        public float GetSueldo() { return _sueldo; }
+        public string GetSexo() { return _sexo; }
+        public float GetPeso() { return _peso; }
+        public float GetAltura() { return _altura; }
+
+        private void CrearEmail()
+        {
+            _email = _nombre + "@superempresa.com";
+        }
+    }
+}
--- SuperEmpresa/Program.cs
+++ SuperEmpresa/Program.cs
@@ -8,8 +8,75 @@ namespace SuperEmpresa
 {
     class Program
     {
+        /* 
+            Consigna:
+            ---------------------
+            1. Pedir por teclado nombre, sueldo, sexo, peso y altura y crear 1 objeto persona.
+            2. Mostrar la información completa de la persona en este formato:
+                -Nombre: Juan
+                -Email: Juan@superempresa.com
+                -sueldo: 30000 ARS
+                -sexo: Masculino
+                -peso:  70 kg(B)
+                -altura: 1,7 m
+        */
+
         static void Main(string[] args)
         {
+            string nombre = MostrarMensaje("Por favor, introduce tu nombre.");
+            float sueldo = ValidarFloat(MostrarMensaje("Introduce tu sueldo (ARS)."));
+            string sexo = MostrarMensaje("Introduce to sexo. (Masculino/Femenino)");
+            float peso = ValidarFloat(MostrarMensaje("Introduce tu peso (kg)."));
+            float altura = ValidarFloat(MostrarMensaje("Introduce tu altura (m)."));
+
+            Persona persona = new Persona(nombre, sueldo, sexo, peso, altura);
+
+            MostrarMensaje("Presiona enter para ver los datos introducidos.");
+
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("- Nombre: " + persona.GetNombre());
+            Console.WriteLine("- Email: " + persona.GetEmail());
+            Console.WriteLine("- Sueldo: " + persona.GetSueldo() + " ARS");
+            Console.WriteLine("- Sexo: " + persona.GetSexo());
+            Console.WriteLine("- Peso: " + persona.GetPeso() + " kg(B)");
+            Console.WriteLine("- Altura: " + persona.GetAltura() + " m");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Listo.");
+            Console.ReadKey();
+        }
+
+        static string MostrarMensaje(string mensaje)
+        {
+            Console.Clear();
+
+            Console.WriteLine("/////////////// TP3 - Ejercicio: SuperEmpresa - Julián B. Ortega ///////////////");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(mensaje);
+            Console.WriteLine();
+            Console.WriteLine("------------------------- ");
+
+            return Console.ReadLine();
+        }
+
+        static float ValidarFloat(string input)
+        {
+            bool exito = false;
+
+            if (!float.TryParse(input, out float resultado))
+            {
+                do
+                {
+                    input = MostrarMensaje("Entrada inválida. Por favor, introduzca un número.");
+                    exito = float.TryParse(input, out resultado);
+                } while (!exito);
+            }
+
+            return resultado;
         }
     }
 }
